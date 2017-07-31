1bd07acfc6f3dee7e3a91ac4d8d378ea74e993e7
Oliver Giles <ohw.giles@gmail.com>
2017-07-31 08:58:06 +0300

    update dependencies to latest versions

--- docker-build-centos.sh
+++ docker-build-centos.sh
@@ -8,35 +8,36 @@ SOURCE_DIR=$(readlink -f $(dirname ${BASH_SOURCE[0]}))
 
 docker run --rm -i -v $SOURCE_DIR:/root/rpmbuild/SOURCES/laminar-$VERSION:ro -v $OUTPUT_DIR:/output centos bash -xe <<EOS
 
-yum -y install rpm-build cmake make gcc gcc-c++ wget sqlite-devel boost-devel zlib-devel
+yum -y install epel-release
+yum -y install rpm-build cmake3 make gcc gcc-c++ wget sqlite-devel boost-devel zlib-devel
 
 mkdir /build
 cd /build
 
-wget -O capnproto.tar.gz https://github.com/sandstorm-io/capnproto/archive/v0.5.3.tar.gz
-wget -O websocketpp.tar.gz https://github.com/zaphoyd/websocketpp/archive/0.6.0.tar.gz
-wget -O rapidjson.tar.gz https://github.com/miloyip/rapidjson/archive/v1.0.2.tar.gz
+wget -O capnproto.tar.gz https://github.com/capnproto/capnproto/archive/v0.6.1.tar.gz
+wget -O websocketpp.tar.gz https://github.com/zaphoyd/websocketpp/archive/0.7.0.tar.gz
+wget -O rapidjson.tar.gz https://github.com/miloyip/rapidjson/archive/v1.1.0.tar.gz
 md5sum -c <<EOF
-909bd13ad6b8bc840ac78ab8f5bcb0a4  capnproto.tar.gz
-5a485884c01f881aafbf1e055d851b82  websocketpp.tar.gz
-97cc60d01282a968474c97f60714828c  rapidjson.tar.gz
+481d0f2fe24f46ce25234ea9704ef0b4  capnproto.tar.gz
+5027c20cde76fdaef83a74acfcf98e23  websocketpp.tar.gz
+badd12c511e081fec6c89c43a7027bce  rapidjson.tar.gz
 EOF
 
 tar xzf capnproto.tar.gz
 tar xzf websocketpp.tar.gz
 tar xzf rapidjson.tar.gz
 
-cd /build/capnproto-0.5.3/c++/
-cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=off .
+cd /build/capnproto-0.6.1/c++/
+cmake3 -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=off .
 make -j4
 make install
 
-cd /build/websocketpp-0.6.0/
-cmake .
+cd /build/websocketpp-0.7.0/
+cmake3 .
 make install
 
-cd /build/rapidjson-1.0.2/
-cmake -DRAPIDJSON_BUILD_EXAMPLES=off .
+cd /build/rapidjson-1.1.0/
+cmake3 -DRAPIDJSON_BUILD_EXAMPLES=off .
 make install
 
 cd
@@ -55,7 +56,7 @@ Lightweight Continuous Integration Service
 %prep
 
 %build
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/ %{_sourcedir}/laminar-$VERSION
+cmake3 -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/ %{_sourcedir}/laminar-$VERSION
 pwd
 make
 
--- docker-build-debian.sh
+++ docker-build-debian.sh
@@ -9,34 +9,34 @@ SOURCE_DIR=$(readlink -f $(dirname ${BASH_SOURCE[0]}))
 docker run --rm -i -v $SOURCE_DIR:/laminar:ro -v $OUTPUT_DIR:/output debian:stable bash -xe <<EOS
 
 apt-get update
-apt-get install -y wget cmake g++ libsqlite3-dev libboost-filesystem1.55-dev zlib1g-dev
+apt-get install -y wget cmake g++ libsqlite3-dev libboost-filesystem1.62-dev zlib1g-dev
 
 mkdir /build
 cd /build
 
-wget -O capnproto.tar.gz https://github.com/sandstorm-io/capnproto/archive/v0.5.3.tar.gz
-wget -O websocketpp.tar.gz https://github.com/zaphoyd/websocketpp/archive/0.6.0.tar.gz
-wget -O rapidjson.tar.gz https://github.com/miloyip/rapidjson/archive/v1.0.2.tar.gz
+wget -O capnproto.tar.gz https://github.com/capnproto/capnproto/archive/v0.6.1.tar.gz
+wget -O websocketpp.tar.gz https://github.com/zaphoyd/websocketpp/archive/0.7.0.tar.gz
+wget -O rapidjson.tar.gz https://github.com/miloyip/rapidjson/archive/v1.1.0.tar.gz
 md5sum -c <<EOF
-909bd13ad6b8bc840ac78ab8f5bcb0a4  capnproto.tar.gz
-5a485884c01f881aafbf1e055d851b82  websocketpp.tar.gz
-97cc60d01282a968474c97f60714828c  rapidjson.tar.gz
+481d0f2fe24f46ce25234ea9704ef0b4  capnproto.tar.gz
+5027c20cde76fdaef83a74acfcf98e23  websocketpp.tar.gz
+badd12c511e081fec6c89c43a7027bce  rapidjson.tar.gz
 EOF
 
 tar xzf capnproto.tar.gz
 tar xzf websocketpp.tar.gz
 tar xzf rapidjson.tar.gz
 
-cd /build/capnproto-0.5.3/c++/
+cd /build/capnproto-0.6.1/c++/
 cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=off .
 make -j4
 make install
 
-cd /build/websocketpp-0.6.0/
+cd /build/websocketpp-0.7.0/
 cmake .
 make install
 
-cd /build/rapidjson-1.0.2/
+cd /build/rapidjson-1.1.0/
 cmake -DRAPIDJSON_BUILD_EXAMPLES=off .
 make install
 
@@ -54,7 +54,7 @@ Section:
 Priority: optional
 Architecture: amd64
 Maintainer: Oliver Giles <web ohwg net>
-Depends: libboost-filesystem1.55.0, zlib1g
+Depends: libboost-filesystem1.62.0, zlib1g
 Description: Lightweight Continuous Integration Service
 EOF
 cat <<EOF > laminar/DEBIAN/postinst
